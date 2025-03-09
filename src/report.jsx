6d7ae9fc37ea65c3b6931b32bac5db05ba51eaec
infice <67130035@dpu.ac.th>
2025-03-09 11:50:27 +0700

    setup gh-pages

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>{/* Name */}</div>
+            <div>{"67130035 Netchanok Khandee"}</div>
           </Container>
         </Navbar>
         <Container>
